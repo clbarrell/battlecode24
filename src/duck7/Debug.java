0a4a7a60e5a507de9854720dd58f0fc6a72eafca
Chris <clbarrell@gmail.com>
2024-01-16 12:29:47 +1100

    duck7 init

---
+++ src/duck7/Debug.java
@@ -0,0 +1,30 @@
+package duck7;
+
+import battlecode.common.MapLocation;
+
+public class Debug extends RobotPlayer {
+  public static void log(String s) {
+    if (rc.hasFlag()) {
+      System.out.println(myLocation.toString() + " " + s);
+
+    }
+  }
+
+  public static void logId(String s, int id) {
+    System.out.println(id + ": " + s);
+  }
+
+  public static String printMapLocations(MapLocation[] locs) {
+    String s = "";
+    for (MapLocation loc : locs) {
+      if (loc != null) {
+        s += loc.toString() + " ";
+      } else {
+        s += "null ";
+      }
+    }
+    return s;
+
+  }
+
+}
